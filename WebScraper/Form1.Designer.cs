e5e0522e91bbcb901f393222a9523be68498ed78
Fangornibro <kuzmindima2002@gmail.com>
2022-08-08 00:28:04 +0300

    Data base
    Data base connection. Saving the result for all dates.

--- WebScraper/Form1.Designer.cs
+++ WebScraper/Form1.Designer.cs
@@ -29,13 +29,16 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.mainTable = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            ((System.ComponentModel.ISupportInitialize)(this.mainTable)).BeginInit();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 523);
+            this.button1.Location = new System.Drawing.Point(12, 639);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -43,24 +46,47 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // dataGridView1
+            // tabPage1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(898, 505);
-            this.dataGridView1.TabIndex = 1;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(934, 0);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // mainTable
+            // 
+            this.mainTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainTable.Location = new System.Drawing.Point(31, 92);
+            this.mainTable.Name = "mainTable";
+            this.mainTable.RowHeadersVisible = false;
+            this.mainTable.Size = new System.Drawing.Size(781, 500);
+            this.mainTable.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(1, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(942, 20);
+            this.tabControl1.TabIndex = 2;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(942, 674);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.mainTable);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainTable)).EndInit();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -68,7 +94,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView mainTable;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
 
--- WebScraper/Form1.cs
+++ WebScraper/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using HtmlAgilityPack;
+using MySql.Data.MySqlClient;
 
 namespace WebScraper
 {
@@ -16,17 +17,85 @@ namespace WebScraper
         public Form1()
         {
             InitializeComponent();
+            tabControlUpdate();
         }
 
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<string> TableNames = GetTables();
+            if (tabControl1.SelectedIndex >= 0)
+            {
+                showData(TableNames[tabControl1.SelectedIndex]);
+            }
+        }
+        private void tabControlUpdate()
+        {
+            List<string> TableNames = GetTables();
+            for (int i = 0; i < tabControl1.TabPages.Count; i++)
+            {
+                tabControl1.TabPages.Remove(tabControl1.TabPages[i]);
+            }
+            for (int i = 0; i < TableNames.Count; i++)
+            {
+                tabControl1.TabPages.Add(TableNames[i]);
+            }            
+            showData(TableNames[tabControl1.SelectedIndex]);
+        }
+        private void showData(string tableName)
+        {
+
+            DB db = new DB();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter();
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM `"+ tableName + "`", db.getConnection());
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            mainTable.DataSource = dt;
+        }
+
+        public List<string> GetTables()
+        {
+            DB db = new DB();
+            List<string> TableNames = new List<string>();
+            MySqlCommand cmd = new MySqlCommand("show tables", db.getConnection());
+            db.openConnection();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                TableNames.Add(reader.GetString(0));
+            }
+            reader.Close();
+            db.closeConnection();
+            return TableNames;
+        }
+        private void insertProduct(Product product, string name)
+        {
+            DB db = new DB();
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO " + "`" + name + "`" + " (`Link`, `ProductCondition`, `Name`, `RegularPrice`, `PromoPrice`, `NumberOfRatings`, `NumberOfQuestions`) VALUES(@L, @PC, @N, @RP, @PP, @NR, @NQ)", db.getConnection());
+            cmd.Parameters.Add("@L", MySqlDbType.VarChar).Value = product.Link;
+            cmd.Parameters.Add("@PC", MySqlDbType.VarChar).Value = product.ProductCondition;
+            cmd.Parameters.Add("@N", MySqlDbType.VarChar).Value = product.Name;
+            cmd.Parameters.Add("@RP", MySqlDbType.Double).Value = product.RegularPrice;
+            cmd.Parameters.Add("@PP", MySqlDbType.Double).Value = product.PromoPrice;
+            cmd.Parameters.Add("@NR", MySqlDbType.Int64).Value = product.NumberOfRatings;
+            cmd.Parameters.Add("@NQ", MySqlDbType.Int64).Value = product.NumberOfQuestions;
+
+            db.openConnection();
+
+            cmd.ExecuteNonQuery();
+
+            db.closeConnection();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             string url = "https://bt.rozetka.com.ua/ua/grills/c81235/";
             List<string> links = GetLinks(url);
             List<Product> products = GetProducts(links);
-            dataGridView1.DataSource = products;
+            CreateTable(products);
+            //showData();
         }
 
-        private static List<Product> GetProducts(List<string> links)
+        private List<Product> GetProducts(List<string> links)
         {
             List<Product> products = new List<Product>();
             foreach (string link in links)
@@ -38,15 +107,15 @@ namespace WebScraper
                 //Condition
                 if (doc.DocumentNode.SelectSingleNode("//p[contains(@class, 'status-label status-label--green')]") != null)
                 {
-                    product.Condition = "Є в наявності";
+                    product.ProductCondition = "Є в наявності";
                 }
                 else if (doc.DocumentNode.SelectSingleNode("//p[contains(@class, 'status-label status-label--orange')]") != null)
                 {
-                    product.Condition = "Закінчується";
+                    product.ProductCondition = "Закінчується";
                 }
                 else if(doc.DocumentNode.SelectSingleNode("//p[contains(@class, 'status-label status-label--gray')]") != null)
                 {
-                    product.Condition = "Немає в наявності";
+                    product.ProductCondition = "Немає в наявності";
                 }
                 //Name
                 product.Name = doc.DocumentNode.SelectSingleNode("//h1[contains(@class, 'product__title')]").InnerText;
@@ -105,7 +174,25 @@ namespace WebScraper
             return products;
         }
 
-        static List<string> GetLinks(string Url)
+        private void CreateTable(List<Product> products)
+        {
+            DB db = new DB();
+            string name = DateTime.Today.ToString();
+            MySqlCommand cmd = new MySqlCommand("CREATE TABLE " + "`" + name + "`" + " ( `Link` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `ProductCondition` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `Name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `RegularPrice` DOUBLE UNSIGNED NOT NULL , `PromoPrice` DOUBLE UNSIGNED NOT NULL , `NumberOfRatings` INT(10) UNSIGNED NOT NULL , `NumberOfQuestions` INT(10) UNSIGNED NOT NULL ) ENGINE = MyISAM CHARSET=utf8 COLLATE utf8_general_ci;", db.getConnection());
+
+            db.openConnection();
+
+            cmd.ExecuteNonQuery();
+
+            db.closeConnection();
+            foreach (Product product in products)
+            {
+                insertProduct(product, name);
+            }
+            tabControlUpdate();
+        }
+
+        private List<string> GetLinks(string Url)
         {
             string newUrl;
             List<string> links = new List<string>();
@@ -127,7 +214,7 @@ namespace WebScraper
             return links;
         }
 
-        static HtmlAgilityPack.HtmlDocument GetDocument(string Url)
+        private HtmlAgilityPack.HtmlDocument GetDocument(string Url)
         {
             HtmlWeb web = new HtmlWeb();
             HtmlAgilityPack.HtmlDocument Doc = web.Load(Url);
@@ -138,7 +225,7 @@ namespace WebScraper
     class Product
     {
         public string Link { get; set; }
-        public string Condition { get; set; }
+        public string ProductCondition { get; set; }
         public string Name { get; set; }
         public double RegularPrice { get; set; }
         public double PromoPrice { get; set; }
